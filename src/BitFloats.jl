ebe82cb61b1bd543e1c6c02fc1f4e1a319b6fa28
Rafael Fourquet <fourquet.rafael@gmail.com>
2018-09-13 15:27:15 +0200

    add UInt80 definition

--- src/BitFloats.jl
+++ src/BitFloats.jl
@@ -4,11 +4,15 @@ module BitFloats
 
 export Float80, Float128
 
+import BitIntegers
+
 
 # * definitions
 
 primitive type Float80  <: AbstractFloat 80  end
 primitive type Float128 <: AbstractFloat 128 end
+
+BitIntegers.@define_integers 80
 
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,17 @@
 using BitFloats, Test
+using BitFloats: UInt80
 
 @testset "definitions" begin
     @test @isdefined Float80
+    @test @isdefined UInt80
     @test @isdefined Float128
     @test sizeof(Float80)  == 10
+    @test sizeof(UInt80)   == 10
     @test sizeof(Float128) == 16
     @test Float80  <: AbstractFloat
+    @test UInt80   <: Unsigned
     @test Float128 <: AbstractFloat
+    @test isprimitivetype(Float80)
+    @test isprimitivetype(UInt80)
+    @test isprimitivetype(Float128)
 end
